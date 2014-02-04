94de738a818d6b964efe146dd4677501ab966380
Pat Paasch <ppaasch@gfxi.com>
2014-02-04 14:46:10 -0600

    simple working version

--- settings.py
+++ settings.py
@@ -10,14 +10,27 @@ DATABASES['native'] = DATABASES['default']
 DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
 AUTOLOAD_SITECONF = 'indexes'
 
+SITE_NAME = 'Badcomputering'
+SITE_DESCRIPTION = 'computering badly'
+SITE_COPYRIGHT = ''
+
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 INSTALLED_APPS = (
     'django.contrib.admin',
-    'django.contrib.contenttypes',
     'django.contrib.auth',
     'django.contrib.sessions',
+    'django.contrib.contenttypes',
+    'django.contrib.sitemaps',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'urlrouter',
+    'minicms',
+    'blog',
+    'disqus',
     'djangotoolbox',
+    'mediagenerator',
+    'simplesocial',
     'autoload',
     'dbindexer',
 
@@ -25,19 +38,37 @@ INSTALLED_APPS = (
     'djangoappengine',
 )
 
+REST_BACKENDS = (
+    'minicms.markup_highlight',
+    'blog.markup_posts',
+)
+
 MIDDLEWARE_CLASSES = (
     # This loads the index definitions, so it has to come first
     'autoload.middleware.AutoloadMiddleware',
 
+    'mediagenerator.middleware.MediaMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+    'djangotoolbox.middleware.RedirectMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'urlrouter.middleware.URLRouterFallbackMiddleware',
+)
+
+URL_ROUTE_HANDLERS = (
+    'minicms.urlroutes.PageRoutes',
+    'blog.urlroutes.BlogRoutes',
+    'blog.urlroutes.BlogPostRoutes',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'minicms.context_processors.cms',
 )
 
 # This test runner captures stdout and associates tracebacks with their
@@ -49,3 +80,5 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
 
 ROOT_URLCONF = 'urls'
+
+NON_REDIRECTED_PATHS = ('/admin/',)
