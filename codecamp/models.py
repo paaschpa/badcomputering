d0b9e5bc5c7d3312c2db8ba81bd07141ae49667e
paaschpa <paaschpa@gmail.com>
2014-03-12 22:59:56 -0500

    add to models presenter layout stuff

--- codecamp/models.py
+++ codecamp/models.py
@@ -12,7 +12,13 @@ class Event(models.Model):
 class Speaker(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    event = models.ForeignKey(Event, related_name='speakers')
+    #event = models.ForeignKey(Event, related_name='speakers')
+    email = models.CharField(max_length=100, blank=True)
+    profile_image = models.CharField(max_length=255, blank=True)
+    website = models.CharField(max_length=100, blank=True)
+    twitter = models.CharField(max_length=100, blank=True)
+    linkedin = models.CharField(max_length=100, blank=True)
+    biography = models.TextField(max_length=1000)
 
     def __unicode__(self):
         return self.first_name + self.last_name
@@ -20,8 +26,11 @@ class Speaker(models.Model):
 
 class Session(models.Model):
     title = models.CharField(max_length=250)
-    summary = models.CharField(max_length=500)
+    abstract = models.CharField(max_length=500)
+    level = models.CharField(max_length=25, choices = ['Beginner', 'Intermediate', 'Advanced', 'All'])
+    keywords = models.CharField(max_length=250, blank=True)
     speaker = models.ForeignKey(Speaker, related_name="sessions")
+    event = models.ForeignKey(Event, related_name="sessions")
 
     def __unicode__(self):
         return self.title
